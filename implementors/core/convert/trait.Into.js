08ad81172971fe90092a21c5cef0a774603a2f19
Travis CI User <travis@example.org>
2017-09-07 09:02:22 +0000

    Documentation for durch/rust-s3@bf10a01

---
+++ implementors/core/convert/trait.Into.js
@@ -0,0 +1,20 @@
+(function() {var implementors = {};
+implementors["backtrace"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/alloc/vec/struct.Vec.html\" title=\"struct alloc::vec::Vec\">Vec</a>&lt;<a class=\"struct\" href=\"backtrace/struct.BacktraceFrame.html\" title=\"struct backtrace::BacktraceFrame\">BacktraceFrame</a>&gt;&gt; for <a class=\"struct\" href=\"backtrace/struct.Backtrace.html\" title=\"struct backtrace::Backtrace\">Backtrace</a>",];
+implementors["chrono"] = [];
+implementors["error_chain"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/alloc/vec/struct.Vec.html\" title=\"struct alloc::vec::Vec\">Vec</a>&lt;<a class=\"struct\" href=\"backtrace/capture/struct.BacktraceFrame.html\" title=\"struct backtrace::capture::BacktraceFrame\">BacktraceFrame</a>&gt;&gt; for <a class=\"struct\" href=\"error_chain/struct.Backtrace.html\" title=\"struct error_chain::Backtrace\">Backtrace</a>",];
+implementors["hmac"] = [];
+implementors["libc"] = [];
+implementors["num"] = [];
+implementors["serde"] = [];
+implementors["serde_xml_rs"] = [];
+implementors["sha2"] = [];
+implementors["syn"] = [];
+implementors["unicode_bidi"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.u8.html\">u8</a>&gt; for <a class=\"struct\" href=\"unicode_bidi/level/struct.Level.html\" title=\"struct unicode_bidi::level::Level\">Level</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
